190aae2069f7a8960160d204309083262cb905b6
Juan Cruz Betancor <juan.betancor@hotmail.com>
2023-04-13 15:15:14 -0300

    subiendo arreglos del proyecto final

--- src/SearchElements.jsx
+++ src/SearchElements.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import {addDoc, collection, docs, getDocs, getFirestore} from 'firebase/firestore'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import './search.css'
 import { CartContext } from './Context/CartContext'
 
@@ -26,24 +26,34 @@ const SearchElements = () => {
     
 return (
     <div className='contenedor-search'>
-        {filteredElementsArray.map(element => (
-        <div key={element.id}>
-            <div class="card-fluid">
-            <div class="card">
-            <img src={element.image} class="img-fluid rounded-start " alt={element.title}/>
-            <div class="card-body">
-            <h5 class="card-title">{element.title}</h5>
-            <p class="card-text">{element.description}</p>
-            <p class="card-text"><small class="text-muted">Talle: {element.waist}</small></p>
-            <p class="card-text">Precio: ${element.price}</p>
-            <button type='button' id={element.id} class="btn btn-primary agregar botones" onClick={() => addToCart(element, 1)}>Comprar
-            </button>
+    {filteredElementsArray.length > 0 ? (
+        filteredElementsArray.map(element => (
+            <div key={element.id}>
+                <div className="card-fluid">
+                    <div className="card">
+                        <img src={element.image} className="img-fluid rounded-start " alt={element.title} />
+                        <div className="card-body">
+                            <h5 className="card-title">{element.title}</h5>
+                            <p className="card-text">{element.description}</p>
+                            <p className="card-text"><small className="text-muted">Talle: {element.waist}</small></p>
+                            <p className="card-text">Precio: ${element.price}</p>
+                            <button type='button' id={element.id} className="btn btn-primary agregar botones" onClick={() => addToCart(element, 1)}>Comprar</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+        ))
+    ) : (
+        <div>
+        <div className="alert alert-danger alert-search">No se encontraron resultados. Por favor vuelva a intertar con algunos de los valores mencionados. Como por ej: camperas, medias y remeras</div>
+        <div className='contenedorSearch'>
+        <Link to={'/'}>
+                <button className='btn btn-primary boton-search'>Ver Catalogo</button>
+        </Link>
         </div>
         </div>
-        </div>
-    ))}
-    </div>
+    )}
+</div>
 )
 }
 
